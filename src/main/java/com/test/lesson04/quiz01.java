95950fd1ff1f625feec8b4484a6d5b4b6f7078c0
test <test@test.com>
2022-12-15 18:46:34 +0900

    학원 컴퓨터 22/12/15 .classpath 올리지말기 - sql 연동 예제 및 퀴즈

---
+++ src/main/java/com/test/lesson04/quiz01.java
@@ -0,0 +1,51 @@
+package com.test.lesson04;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.test.common.MysqlService;
+
+@WebServlet("/lesson04/quiz01")
+public class quiz01 extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/plain");
+		
+		// DB 연결
+		MysqlService ms = MysqlService.getInstance();
+		ms.connect(); // 꼭 넣자
+		
+		// DB insert 한 행
+		String insertQuery = "insert into `real_estate`"
+				+ "(`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`)"
+				+ "values"
+				+ "(3, '헤라펠리스 101동 5305호', 350, '매매', 1500000, null)";
+		
+		try {
+			ms.update(insertQuery);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// DB select & 결과 출력
+		PrintWriter out = response.getWriter();
+		String selectQuery = "select * from `real_estate`";
+		try {
+			ResultSet resultSet = ms.select(selectQuery);
+			while (resultSet.next()) { // 결과행이 있는 동안 수행된다.
+				out.println(resultSet.getInt("id"));
+				out.println(resultSet.getString("address"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
